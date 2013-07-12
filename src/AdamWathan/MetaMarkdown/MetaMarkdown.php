e7f7f0e89ea38012bc2ea93a9adda7eb360b28f7
Adam Wathan <adam.wathan@gmail.com>
2013-07-11 22:51:49 -0400

    Working, quick, dirty and ugly

--- src/AdamWathan/MetaMarkdown/MetaMarkdown.php
+++ src/AdamWathan/MetaMarkdown/MetaMarkdown.php
@@ -1,9 +1,54 @@
-<?php namespace AdamWathan\MetaMarkdown;
+<?php 
+
+namespace AdamWathan\MetaMarkdown;
+
+use dflydev\markdown\MarkdownParser;
 
 class MetaMarkdown
 {
-	public function test()
+	private $raw;
+
+	public $html;
+
+	public function __construct($path)
 	{
-		return "test";
+		$this->raw = file_get_contents($path);
+		$this->extractMetadata();
+		$this->compileHTML();
+	}
+
+	private function extractMetadata()
+	{
+		$pattern = '/^(.+:\s+.+\n)+([\s\S]*)/';
+		preg_match($pattern, $this->raw, $matches);
+		
+		$metaData = explode("\n", $matches[0]);
+		$this->raw = $matches[2];
+		
+		$this->parseMetaData($metaData);
+	}
+
+	private function parseMetaData($metaData)
+	{
+		foreach($metaData as $data) {
+			$this->parseKeyValue($data);
+		}
+	}
+
+	private function parseKeyValue($pair)
+	{
+		$matches = array();
+		$pattern = '/^(.+):\s+(.+)$/';
+		preg_match($pattern, $pair, $matches);
+
+		if(count($matches)) {
+			$this->{strtolower($matches[1])} = $matches[2];
+		}
+	}
+
+	private function compileHTML()
+	{
+		$markdown = new MarkdownParser();
+		$this->html = $markdown->transformMarkdown($this->raw);
 	}
 }
